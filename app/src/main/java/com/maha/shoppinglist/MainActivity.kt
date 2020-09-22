baba4664759ce69c78b39e409e8e2980e646f0aa
MartinHalus <halus.martin@gmail.com>
2020-09-22 12:39:25 +0200

    change 3

--- app/src/main/java/com/maha/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/maha/shoppinglist/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
-// experimental branch 3
+// experimental branch 3  added something
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
